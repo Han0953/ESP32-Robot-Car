50c3a2920a2687195df383917c128c5a5652e197
Rehan <muhammadrehan302005@gmail.com>
2026-08-06 22:19:31 +0700

    Update MotorController.cpp

--- firmware/ESP32_robot_car/MotorController.cpp
+++ firmware/ESP32_robot_car/MotorController.cpp
@@ -1,3 +1,71 @@
 #include "MotorController.h"
 
 MotorController::MotorController() : motorSpeed(128) {}
+
+void MotorController::begin() {
+    pinMode(ENA, OUTPUT);
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(ENB, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    stop();
+}
+
+void MotorController::setSpeed(int speed) {
+    motorSpeed = speed;
+    Serial.print("Variabel kecepatan diatur ke: ");
+    Serial.println(motorSpeed);
+}
+
+int MotorController::getSpeed() const {
+    return motorSpeed;
+}
+
+void MotorController::stop() {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+    Serial.println("Perintah: Berhenti");
+}
+
+void MotorController::moveForward() {
+    analogWrite(ENA, motorSpeed);
+    analogWrite(ENB, motorSpeed);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    Serial.println("Perintah: Maju");
+}
+
+void MotorController::moveBackward() {
+    analogWrite(ENA, motorSpeed);
+    analogWrite(ENB, motorSpeed);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    Serial.println("Perintah: Mundur");
+}
+
+void MotorController::turnLeft() {
+    analogWrite(ENA, motorSpeed);
+    analogWrite(ENB, motorSpeed);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    Serial.println("Perintah: Belok Kiri");
+}
+
+void MotorController::turnRight() {
+    analogWrite(ENA, motorSpeed);
+    analogWrite(ENB, motorSpeed);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    Serial.println("Perintah: Belok Kanan");
+}
